2a29956f19b74934c0839b503a7c96dbbd2b5a1c
rezmayyy <austinmann12@gmail.com>
2024-10-13 10:31:37 -0700

    Add search bar. Create navcenter div for items in center.

--- wellness-app/src/assets/Header.jsx
+++ wellness-app/src/assets/Header.jsx
@@ -1,26 +1,31 @@
 import { Link } from 'react-router-dom';
 import React from 'react';
 import '../styles/header.css';
+import logo from '../assets/Logo.png';
 
 
 function Header() {
 
     return (
         <header>
-            <h1>TribeWell</h1>
+            <div className="brand-container">
+                <img src={logo} alt="TribeWell Logo" className="logo" />
+                <h1>TribeWell</h1>
+            </div>
             <nav>
+                <div className="nav-center">
+                    <input type="text" placeholder="Search..." className="search-bar" />
+                    <ul className="nav-links">
+                        <li><Link to="/explore">Explore</Link></li>
+                        <li><Link to="/learn">Learn</Link></li>
+                        <li><Link to="/blogs">Blogs</Link></li>
+                    </ul>
+                </div>
                 <ul className="auth-links">
                     <li><Link to="/login">Log In</Link></li>
                     <li><Link to="/signup">Sign Up</Link></li>
                 </ul>
-                <ul className="nav-links">
-                    <li><Link to="/explore">Explore</Link></li>
-                    <li><Link to="/learn">Learn</Link></li>
-                    <li><Link to="/blogs">Blogs</Link></li>
-                </ul>
-
             </nav>
-            <hr></hr>
         </header>
     )
 }
