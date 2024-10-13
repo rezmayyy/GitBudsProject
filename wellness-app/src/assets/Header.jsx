8bc90c0c7809d773abdd3ac2a68df2e05e8a7469
bistromath0 <aerlandson@csus.edu>
2024-10-13 16:07:15 -0700

    Added profile page

--- wellness-app/src/assets/Header.jsx
+++ wellness-app/src/assets/Header.jsx
@@ -13,6 +13,7 @@ function Header(){
                     <li><Link to="/explore">Explore</Link></li>
                     <li><Link to="/learn">Learn</Link></li>
                     <li><Link to="/blogs">Blogs</Link></li>
+                    <li><Link to="/profile">Profile</Link></li>
                 </ul>
             </nav>
             <hr></hr>
