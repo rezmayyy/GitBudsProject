1ec2d41575a5d6cffcc3e35525e411ef4be0df21
rezmayyy <austinmann12@gmail.com>
2024-10-13 18:34:56 -0700

    Broke main branch. Fixed main branch.

--- wellness-app/src/assets/Login.jsx
+++ wellness-app/src/assets/Login.jsx
@@ -1,13 +1,10 @@
+import React from "react";
+import { Link } from "react-router-dom"; // Keep only one import statement for Link
 import "../styles/login.css";
 import "../styles/guide.css";
-import {FaUser, FaLock} from "react-icons/fa";
-import {Link} from 'react-router-dom';
+import { FaUser, FaLock } from "react-icons/fa";
 
-//import './login.css';
-import React from "react";
-import { Link } from "react-router-dom";
-function Login(){
-
+function Login() {
     const [email, setEmail] = React.useState('');
     const [password, setPassword] = React.useState('');
 
@@ -25,72 +22,51 @@ function Login(){
         console.log('Login attempted with:', email, password);
     };
 
-
-    return(
+    return (
         <div className="Home">
-
-
             <div className="wrapper">
-
                 <div className="form-box login">
-                    <form action="">
+                    <form onSubmit={handleSubmit}>
                         <h1>Login</h1>
                         <div className="input-box">
-                            <input type="text" placeholder="Username" required />
+                            <input
+                                type="text"
+                                placeholder="Username"
+                                required
+                                value={email}
+                                onChange={handleEmailChange}
+                            />
                             <FaUser className="icon" />
                         </div>
                         <div className="input-box">
-                            <input type="password" placeholder="Password" required />
-                            <FaLock className="icon"/>
+                            <input
+                                type="password"
+                                placeholder="Password"
+                                required
+                                value={password}
+                                onChange={handlePasswordChange}
+                            />
+                            <FaLock className="icon" />
                         </div>
-                        
+
                         <div className="remember-forgot">
-                            <label><input type="checkbox"/>Remember me</label>
-                                <Link to="#">Forgot password?</Link>
+                            <label>
+                                <input type="checkbox" /> Remember me
+                            </label>
+                            <Link to="#">Forgot password?</Link>
                         </div>
 
                         <button type="submit">Login</button>
 
                         <div className="register-link">
                             <p>
-                                Don't have an account?
+                                Don't have an account? 
                                 <Link to="/signup"> Sign Up</Link>
                             </p>
                         </div>
-
                     </form>
                 </div>
-
             </div>
-        
-        </div>
-        
-        <div>
-            <h2>Log In Page</h2>
-            <form onSubmit={handleSubmit}>
-            <div>
-                <label htmlFor="email">Email:</label>
-                <input
-                    type="email"
-                    id="email"
-                    value={email}
-                    onChange={handleEmailChange}
-                    required
-                />
-            </div>
-            <div>
-                <label htmlFor="password">Password:</label>
-                <input
-                    type="password"
-                    id="password"
-                    value={password}
-                    onChange={handlePasswordChange}
-                    required
-                />
-            </div>
-            <button type="submit">Log In</button>
-            </form>
-            <p>Don't have an account? <Link to="/signup">Sign up here</Link></p>
         </div>
     );
 }
