98f87917602257313a9b40fe1e0d3928d6beabaf
Jason Neal <jtneal@csus.edu>
2024-10-26 17:51:40 -0700

    Added redirect to login from profile if not logged in

--- wellness-app/src/assets/Header.jsx
+++ wellness-app/src/assets/Header.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import React, {useContext} from 'react';
 import '../styles/header.css';
 import logo from '../assets/Logo.png';
@@ -7,6 +7,14 @@ import Signout from './Signout';
 
 function Header() {
     const {user} = useContext(UserContext);
+    const navigate = useNavigate();
+    //profile login check
+    const handleProfileClick = (event) => {
+        if (!user) {
+            event.preventDefault(); // Prevent default navigation
+            navigate('/login'); // Redirect to login page
+        }
+    };
     return (
         <header>
             {/* Div for Logo and "TribeWell" */}
@@ -23,7 +31,7 @@ function Header() {
                         <li><Link to="/explore">Explore</Link></li>
                         <li><Link to="/learn">Learn</Link></li>
                         <li><Link to="/blogs">Blogs</Link></li>
-                        <li><Link to="/profile">Profile</Link></li>
+                        <li><Link to="/profile" onClick={handleProfileClick}>Profile</Link></li>
                         <li><Link to="/discussion">Discussion Board</Link></li>
 
                     </ul>
