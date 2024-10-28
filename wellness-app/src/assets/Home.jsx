055153cce683dc7ab1e51d15707ceffd0e431759
rezmayyy <austinmann12@gmail.com>
2024-10-28 12:47:02 -0700

    SCRUM-112 Display content on homepage

--- wellness-app/src/assets/Home.jsx
+++ wellness-app/src/assets/Home.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import Subscriptions from './Subscriptions';
 import DiscussionBoard from './DiscussionBoard';
+import RecentVideos from './RecentVideos';
 
 function Home() {
     return (
       <main className="home-container">
       <div id="subscriptions">
         <Subscriptions />
+      </div>
+
+      <div id="recent-videos">
+        <RecentVideos />
       </div>
 
       <div id="discussion-board">
