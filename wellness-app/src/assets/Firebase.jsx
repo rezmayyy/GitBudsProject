d2e56419d5c7ec61286d495c97ee8e9e2fd85224
ParsaBayatt <161535000+ParsaBayatt@users.noreply.github.com>
2024-10-31 21:49:57 -0700

    Updated Firebase.jsx for Profile page

--- wellness-app/src/assets/Firebase.jsx
+++ wellness-app/src/assets/Firebase.jsx
@@ -4,7 +4,7 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-
+import { getStorage } from "firebase/storage";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
