ba3784224251aaf6f11ac06e9cd42c7e795bf90c
Kat Shpak <7cathrine@gmail.com>
2025-03-10 20:32:20 -0700

    SCRUM246: Display articles/videos on blogs page

--- wellness-app/src/assets/Blogs/BlogsPage.jsx
+++ wellness-app/src/assets/Blogs/BlogsPage.jsx
@@ -3,11 +3,16 @@ import { getFirestore, collection, getDocs, query, where, doc, deleteDoc, update
 import { getAuth } from "firebase/auth";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Card, Button, Container, Row, Col, Nav } from "react-bootstrap";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
+import {format} from "date-fns";
 
 
 const BlogsPage = () => {
 
+    const db = getFirestore();
+    const auth = getAuth();
+    const user = auth.currentUser;
+    const navigate = useNavigate();
 
     const [activeTab, setActiveTab] = useState("All topics");
     const [activeCategoryTab, setActiveCategoryTab] = useState("All categories");
@@ -15,9 +20,63 @@ const BlogsPage = () => {
     const topics = ["All topics", "Business Advice", "Healer Q&A", "Insights", "Marketing Tips", "New Features", "Personal Growth"];
     const categories = ["All categories", "Articles", "Videos"];
 
-    useEffect(() => {
+    const [articlePosts, setArticlePosts] = useState([]);
+    const [visibleArticlePosts, setVisibleArticlePosts] = useState(10);
 
-    }, []);
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            //from home page fetch recent videos function
+            try {
+                const postsRef = collection(db, 'content-posts');
+                let q; 
+                
+                
+                
+
+                if(activeCategoryTab === "All categories"){
+                    q = query(
+                        postsRef,
+                        where('status', '==', 'approved'),
+                    );
+                }else if(activeCategoryTab === "Articles"){
+                    q = query(
+                        postsRef,
+                        where('status', '==', 'approved'),
+                        where('type', '==', 'article')
+                    );
+                }else if(activeCategoryTab === "Videos"){
+                    q = query(
+                        postsRef,
+                        where('status', '==', 'approved'),
+                        where('type', '==', 'video')
+                    );
+                }
+
+
+                const querySnapshot = await getDocs(q);
+                const fetchedPosts = querySnapshot.docs.map(doc => {
+                    const data = doc.data();
+                    const timestamp = data.timestamp ? data.timestamp.toDate() : null;
+                    return {
+                        id: doc.id,
+                        title: data.title,
+                        url: data.fileURL,
+                        postDate: timestamp ? format(timestamp, "PP p") : "Unknown Date",
+                        thumbnail: data.thumbnailURL || "/assets/default-post-thumbnail.jpg",
+                        author: data.author, // The displayName of the author
+                        type: data.type
+                    }
+                });
+                setArticlePosts(fetchedPosts);
+            } catch (error) {
+                console.error("Error fetching articles:", error)
+            }
+
+        };
+        fetchPosts();
+
+    }, [activeCategoryTab]);
 
     return (
         <Container className="my-5">
@@ -29,114 +88,134 @@ const BlogsPage = () => {
                 backgroundPosition: 'center',
                 backgroundAttachment: 'fixed',
                 overflow: 'hidden',
-                
+
             }}>
-            <Row className="g-0 h-100">
-                <Col md={6} className="d-flex flex-column justify-content-center p-4">
-                    <Card.Body>
-                        <Card.Title className="display-4">Learn, Grow, Heal</Card.Title>
-                        <Card.Title className="display-4">-<span className="text-warning">for Free.</span></Card.Title>
-                        <Card.Text className="lead">Explore free topics that inspire, educate, and empower your journey.</Card.Text>
-                        <div className="d-flex">
-                            <Button variant="primary" className="me-3">Subscribe</Button>
-                            <Button variant="primary">Download our free healing guides</Button>
-                        </div>
-                    </Card.Body>
-                </Col>
-            </Row>
-
-        </Card>
-
-            {/* categories nav */ }
-    <Row>
-        <Col md={3} className="border-end pe-4">
-            <Nav className="flex-column" style={{ marginTop: "70px" }}>
-                {categories.map((tab) => (
-                    <Nav.Item key={tab} className="mb-3" style={{ position: "relative" }}>
-                        <span
-                            className="text-dark"
-                            onClick={() => setActiveCategoryTab(tab)}
-                            style={{
-                                cursor: "pointer",
-                                paddingBottom: "8px",
-                                display: "inline-block",
-                                position: "relative"
-                            }}>
-                            {tab}
-                        </span>
-                        {activeCategoryTab === tab && (
-                            <div style={{
-                                position: "absolute",
-                                bottom: "0",
-                                left: "0",
-                                width: "50%",
-                                height: "2px",
-                                backgroundColor: "black",
-                                transition: "width 0.3s ease-in-out"
-                            }}>
-
+                <Row className="g-0 h-100">
+                    <Col md={6} className="d-flex flex-column justify-content-center p-4">
+                        <Card.Body>
+                            <Card.Title className="display-4">Learn, Grow, Heal</Card.Title>
+                            <Card.Title className="display-4">-<span className="text-warning">for Free.</span></Card.Title>
+                            <Card.Text className="lead">Explore free topics that inspire, educate, and empower your journey.</Card.Text>
+                            <div className="d-flex">
+                                <Button as={Link} to="/directory" variant="primary" className="me-3" 
+                                    style={{backgroundColor: "#5B56A4", borderColor: "#5B56A4"}}>
+                                        Find a Healer
+                                </Button>
+                                {/*<Button variant="primary">Download our free healing guides</Button>*/}
                             </div>
-                        )}
-                    </Nav.Item>
-                ))}
-            </Nav>
-        </Col>
+                        </Card.Body>
+                    </Col>
+                </Row>
 
-        {/* topics nav */}
-        <Col md={9}>
-            <div className="mt-4 position-relative border-bottom">
-                <Nav className="d-flex">
-                    {topics.map((topic) => (
-                        <Nav.Item key={topic} className="me-4" style={{ position: "relative" }}>
-                            <span
-                                className="text-dark d-inline-block pb-2"
-                                onClick={() => setActiveTab(topic)}
-                                style={{ cursor: "pointer", position: "relative" }}>
+            </Card>
 
-                                {topic}
-                            </span>
-                            {activeTab === topic && (
-                                <div className="position-absolute bottom-0 start-50 translate-middle-x"
+            {/* categories nav */}
+            <Row>
+                <Col md={3} className="border-0 pe-4">
+                    <Nav className="flex-column" style={{ marginTop: "70px" }}>
+                        {categories.map((tab) => (
+                            <Nav.Item key={tab} className="mb-3" style={{ position: "relative" }}>
+                                <span
+                                    className="text-dark"
+                                    onClick={() => setActiveCategoryTab(tab)}
                                     style={{
-                                        width: "100%", height: "2px", backgroundColor: "black"
-                                    }}></div>
-                            )}
+                                        cursor: "pointer",
+                                        paddingBottom: "8px",
+                                        display: "inline-block",
+                                        position: "relative"
+                                    }}>
+                                    {tab}
+                                </span>
+                                {activeCategoryTab === tab && (
+                                    <div style={{
+                                        position: "absolute",
+                                        bottom: "0",
+                                        left: "0",
+                                        width: "50%",
+                                        height: "2px",
+                                        backgroundColor: "black",
+                                        transition: "width 0.3s ease-in-out"
+                                    }}>
 
-                        </Nav.Item>
-                    ))}
-                </Nav>
-            </div>
+                                    </div>
+                                )}
+                            </Nav.Item>
+                        ))}
+                    </Nav>
+                </Col>
 
-            {/* content */}
-            <div className="mt-4">
-                <h3>{activeTab} Blogs</h3>
-                <p>Articles</p>
-            </div>
+                {/* topics nav */}
+                <Col md={9}>
+                    <div className="mt-4 position-relative border-bottom">
+                        <Nav className="d-flex">
+                            {topics.map((topic) => (
+                                <Nav.Item key={topic} className="me-4" style={{ position: "relative" }}>
+                                    <span
+                                        className="text-dark d-inline-block pb-2"
+                                        onClick={() => setActiveTab(topic)}
+                                        style={{ cursor: "pointer", position: "relative" }}>
 
+                                        {topic}
+                                    </span>
+                                    {activeTab === topic && (
+                                        <div className="position-absolute bottom-0 start-50 translate-middle-x"
+                                            style={{
+                                                width: "100%", height: "2px", backgroundColor: "black"
+                                            }}></div>
+                                    )}
 
-            <Row className="mt-4">
-                <Col md={12}>
-                    <Card className="mb-4" style={{ borderRadius: "15px" }}>
-                        <Row className="g-0">
-                            <Col md={4}>
-                                <Card.Img src="" alt="" className="img-fluid rounded-start" />
+                                </Nav.Item>
+                            ))}
+                        </Nav>
+                    </div>
+
+                    {/* content */}
+
+                    <Row className="mt-4">
+
+                        {articlePosts.slice(0, visibleArticlePosts).map(post => (
+
+                            <Col md={12}>
+                                <Card className="mb-4 border-0 hover-card" style={{ borderRadius: "15px", transition: "transform 0.3s ease" }}>
+                                    <Row className="g-0">
+                                        <Col md={4}>
+                                            <Card.Img src={post.thumbnail} alt={post.title} 
+                                                className="img-fluid rounded-start" 
+                                                style={{width: "100%", height: "200px", objectFit: "cover"}}/>
+                                        </Col>
+                                        <Col md={8}>
+                                            <Card.Body>
+                                                <Card.Title className="fw-bold">{post.title}</Card.Title>
+                                                <Card.Text className="text-muted small">{post.postDate}</Card.Text>
+                                                <Card.Text>
+                                                    {/*display text depending on post type*/}
+                                                    {post.type === 'article' ? (
+                                                        <Link to={`/content/${post.id}`} 
+                                                            className="stretched-link" 
+                                                            style={{textDecoration: "none"}}>
+                                                                <span className="text-warning">Read article</span>
+                                                        </Link>                                                 
+                                                    ) : post.type === 'video' ? (
+                                                        <Link to={`/content/${post.id}`} 
+                                                            className="stretched-link" 
+                                                            style={{textDecoration: "none"}}>
+                                                                <span className="text-warning">View video</span>
+                                                        </Link> 
+                                                    ) : null }
+                                                              
+                                                </Card.Text>
+                                            </Card.Body>
+                                        </Col>
+                                    </Row>
+                                </Card>
+                                <hr style={{borderStyle: "dashed", color: "orange"}}/>
                             </Col>
-                            <Col md={8}>
-                                <Card.Body>
-                                    <Card.Title className="fw-bold">Title</Card.Title>
-                                    <Card.Text className="text-muted small">Post date</Card.Text>
-                                    <Card.Text>
-                                        <a href="#" className="stretched-link">Read article</a>
-                                    </Card.Text>
-                                </Card.Body>
-                            </Col>
-                        </Row>
-                    </Card>
+                            
+                        ))}
+                    </Row>
+
                 </Col>
             </Row>
-
-        </Col>
-    </Row>
         </Container >
 
 
