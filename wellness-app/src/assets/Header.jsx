d916c3eba9d0647890a368fc72972e210fcd65ba
Jason Neal <jtneal@csus.edu>
2025-03-11 16:07:13 -0700

    quick re-arrangement of header

--- wellness-app/src/assets/Header.jsx
+++ wellness-app/src/assets/Header.jsx
@@ -196,12 +196,12 @@ function Header() {
         {isMobile && (
           <ul className={styles.hamburgerNavLinks}>
             <li><Link to="/" onClick={closeMenu}>Home</Link></li>
-            <li><Link to="/explore" onClick={closeMenu}>Explore</Link></li>
-            <li><Link to="/contact" onClick={closeMenu}>Contact</Link></li>
+            <li><Link to="/directory" onClick={closeMenu}>Directory</Link></li>
             <li><Link to="/blogs" onClick={closeMenu}>Blogs</Link></li>
+            <li><Link to="/profile" onClick={closeMenu}>Profile</Link></li>
             <li><Link to="/discussion" onClick={closeMenu}>Discussion Board</Link></li>
             <li><Link to="/create" onClick={closeMenu}>Create</Link></li>
-            <li><Link to="/profile" onClick={closeMenu}>Profile</Link></li>
+            <li><Link to="/contact" onClick={closeMenu}>Contact</Link></li>
           </ul>
         )}
 
